fbdfee33de172dc505025b706937628f8e1f1a07
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-07-19 11:46:45 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose')
 const bodyParser = require('koa-bodyparser')
 const initMongoose = require('./models/initMongoose')
 require('./models/user')
+require('./models/post')
 
-const { User } = mongoose.models
+const { User, Post } = mongoose.models
 
 const app = new Koa()
 const r = new Router()
@@ -50,6 +51,36 @@ r.put('/users/:id', async ctx => {
   const { body } = ctx.request
   const user = await User.findByIdAndUpdate(id, body, { new: true })
   ctx.body = user
+})
+
+r.get('/posts', async ctx => {
+  const posts = await Post.find()
+  ctx.body = posts
+})
+
+r.get('/posts/:id', async ctx => {
+  const { id } = ctx.params
+  const post = await Post.findById(id)
+  ctx.body = post
+})
+
+r.put('/posts/:id', async ctx => {
+  const { body } = ctx.request
+  const { id } = ctx.params
+  const post = await Post.findByIdAndUpdate(id, body, { new: true })
+  ctx.body = post
+})
+
+r.post('/posts', async ctx => {
+  const { body } = ctx.request
+  const post = await Post.create(body)
+  ctx.body = post
+})
+
+r.del('/posts/:id', async ctx => {
+  const { id } = ctx.params
+  const post = await Post.findByIdAndDelete(id)
+  ctx.body = post
 })
 
 app.use(logger())
