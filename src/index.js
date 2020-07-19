f8d7f9c87d375ea0f6467f19b4209dd6bd3212fd
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-07-19 09:29:11 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world test')
+const mongoose = require('mongoose')
+
+const mongoUrl =
+  'mongodb+srv://paiboon15721:mypassword@cluster0.sqx0p.mongodb.net/blogs?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUrl)
+
+mongoose.connection.once('open', () => {
+  console.log('Database connected')
+})
