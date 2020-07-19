b720f0afae95e6eaae305bba2b962d91dd60bd8d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-07-19 14:03:57 +0700

    update

--- test/models/user.js
+++ test/models/user.js
@@ -2,17 +2,42 @@ const { assert } = require('chai')
 const { User } = require('mongoose').models
 
 describe('User', () => {
-  it('Create user', async () => {
+  it('Create user by helper function', async () => {
     await User.create({
       email: 'test@gmail.com',
       password: 'testpassword',
       name: 'test',
     })
     const users = await User.find()
-    console.log(users)
+    assert.equal(users.length, 1)
   })
 
-  it('Delete user', async () => {
-    assert.equal(true, true)
+  it('Create user by constructor', async () => {
+    const user = new User({
+      email: 'test@gmail.com',
+      password: 'testpassword',
+      name: 'test',
+    })
+    await user.save()
+    const users = await User.find()
+    assert.equal(users.length, 1)
+  })
+
+  it.only('Update user', async () => {
+    const user = new User({
+      email: 'test@gmail.com',
+      password: 'testpassword',
+      name: 'test',
+    })
+    await user.save()
+    const newUser = await User.findByIdAndUpdate(
+      user._id,
+      {
+        password: 'editpassword',
+        name: 'edited name',
+      },
+      { new: true },
+    )
+    assert.equal(newUser.name, 'edited name')
   })
 })
