fef23ad621752c862f0c08a3b371e55df5ff6f00
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-07-19 10:28:26 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const Koa = require('koa')
 const logger = require('koa-logger')
 const Router = require('koa-router')
+const mongoose = require('mongoose')
 require('./models/initMongoose')
+require('./models/user')
+
+const { User } = mongoose.models
 
 const app = new Koa()
 const r = new Router()
 
 r.get('/', ctx => {
   ctx.body = 'hello world'
+})
+
+r.get('/users', async ctx => {
+  const users = await User.find()
+  ctx.body = users
 })
 
 app.use(logger())
